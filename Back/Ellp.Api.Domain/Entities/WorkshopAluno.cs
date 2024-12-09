767821f0402c8b64369520b38b9e380b5e019ddc
Fabio Nascimento dos Santos Junior <fnjunior95@gmail.com>
2024-12-09 00:45:36 -0300

    agora vai

---
+++ Back/Ellp.Api.Domain/Entities/WorkshopAluno.cs
@@ -0,0 +1,12 @@
+
+namespace Ellp.Api.Domain.Entities
+{
+    public class WorkshopAluno
+    {
+        public int WorkshopId { get; set; }
+        public Workshop Workshop { get; set; }
+
+        public int StudentId { get; set; }
+        public Student Student { get; set; }
+    }
+}
