833bd18d72ca49f24986d753ae915df8e5d6d35b
Eduardo Gabriel de Freitas <58999344+Eduardogdfrt@users.noreply.github.com>
2025-02-09 13:33:15 -0300

    Rota pra retornar todos os alunos e o selecionado

---
+++ Back/Ellp.Api.Application/UseCases/Users/SearchAllStudents/SearchAllStudentsInput.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using MediatR;
+
+namespace Ellp.Api.Application.UseCases.Users.SearchAllStudents
+{
+    public class SearchAllStudentsInput : IRequest<IEnumerable<SearchAllStudentsOutput>>
+    {
+        public int? StudentId { get; set; }
+
+        public SearchAllStudentsInput(int? studentId = null)
+        {
+            StudentId = studentId;
+        }
+    }
+}
+
